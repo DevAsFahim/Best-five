20d0a77c407697e5bccfdb53bf9befb3b52e8054
ASFahim007 <devasfahim@gmail.com>
2022-08-22 22:09:00 +0600

    created a function to get input value

---
+++ scripts/app.js
@@ -0,0 +1,28 @@
+function getInputValue(inputId){
+    const inputValue = document.getElementById(inputId);
+    const inputValueString = inputValue.value;
+    const inputValueNumber = parseFloat(inputValueString);
+    inputValue.value = '';
+    return inputValueNumber;
+}
+
+// document.getElementById('calc_player_cost').addEventListener('click', function(){
+//     const playerCost = getInputValue('player_cost')
+//     const playerTotal = document.getElementById('player_total');
+//     if( isNaN(playerCost) ){
+//         return alert('Please insert number')
+//     }
+//     playerTotal.innerText = playerCost * 5;
+// })
+
+// document.getElementById('tatal_cost').addEventListener('click', function(){
+//     const managerCost = getInputValue('manager');
+//     const coachCoast = getInputValue('coach');
+//     if( isNaN(managerCost) || isNaN(coachCoast) ){
+//         return alert('Please insert number')
+//     }
+//     const playerTotal = parseFloat(document.getElementById('player_total').innerText);
+//     const newTotalCost = managerCost + coachCoast + playerTotal;
+//     const PrevTotalCost = document.getElementById('total_cost');
+//     PrevTotalCost.innerText = newTotalCost;
+// })
