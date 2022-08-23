d2158da42255ce78cd1ec2b2d8908658bf3cd9de
ASFahim007 <devasfahim@gmail.com>
2022-08-23 19:22:47 +0600

    player budget count with player number added on the list, player will show on heading of selec list, code organized properly

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,29 @@
+// select player and add them on list
+const selectButtons = document.getElementsByClassName('select_btn');
+const selectedPlayer = document.getElementById('selected_player');
+for(const selectButton of selectButtons){
+    selectButton.addEventListener('click', function(event){
+        const playerName = event.target.parentNode.childNodes[1].innerText;
+        const li = document.createElement('li');
+        li.innerText = playerName;
+        li.classList.add('mb-3')
+        // condition to add no more than five players
+        if(selectedPlayer.childElementCount > 4){
+            return alert("You can't add more than five name")
+        }
+        selectedPlayer.appendChild(li);
+        // disable button on click
+        event.target.disabled = true;
+        const playerCount = document.getElementById('player_count');
+        playerCount.innerText = selectedPlayer.childElementCount;
+    })
+}
+
+
+
+// calculating player Expenses
+
+// function to get input value
 function getInputValue(inputId){
     const inputValue = document.getElementById(inputId);
     const inputValueString = inputValue.value;
@@ -5,17 +31,16 @@ function getInputValue(inputId){
     inputValue.value = '';
     return inputValueNumber;
 }
-
-
+// calculating five player cost
 document.getElementById('calc_player_cost').addEventListener('click', function(){
     const playerCost = getInputValue('player_cost')
     const playerTotal = document.getElementById('player_total');
     if( isNaN(playerCost) ){
         return alert('Please insert number')
     }
-    playerTotal.innerText = playerCost * 5;
+    playerTotal.innerText = playerCost * selectedPlayer.childElementCount;
 })
-
+// calculating total cost
 document.getElementById('tatal_cost').addEventListener('click', function(){
     const managerCost = getInputValue('manager');
     const coachCoast = getInputValue('coach');
@@ -29,18 +54,3 @@ document.getElementById('tatal_cost').addEventListener('click', function(){
 })
 
 
-const selectButtons = document.getElementsByClassName('select_btn');
-for(const selectButton of selectButtons){
-    selectButton.addEventListener('click', function(event){
-        const playerName = event.target.parentNode.childNodes[1].innerText;
-        const selectedPlayer = document.getElementById('selected_player');
-        const li = document.createElement('li');
-        li.innerText = playerName;
-        li.classList.add('mb-3')
-        if(selectedPlayer.childElementCount > 4){
-            return alert("You can't add more than five name")
-        }
-        selectedPlayer.appendChild(li);
-        event.target.disabled = true;
-    })
-}
